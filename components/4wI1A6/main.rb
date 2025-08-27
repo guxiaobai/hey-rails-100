340c31f1a95b4e2c20a8d649c60d7cca9c3862ff
guxiaobai <sikuan.gu@gmail.com>
2025-08-27 19:01:03 +0800

    devise

--- components/4wI1A6/main.rb
+++ components/4wI1A6/main.rb
@@ -1,14 +1,42 @@
+def source_paths
+  Array(super) +
+  [File.expand_path('../templates', __FILE__)]
+end
+
 gem 'devise', '~> 4.9', '>= 4.9.4'
 
 run_bundle
 
 
 generate('devise:install')
-generate('devise', 'admin')
-
+generate('devise', 'user')
 
 
 # seeds
+append_to_file 'db/seeds.rb' do
+  <<-EOS.strip_heredoc
+  Admin.create(email: 'os@qq.com', password: '000000')
+  EOS
+end
+
+# 路由定制
+
+# devise_for :admins, only: [:sessions]
+
+# namespace :admin do
+# end
+
+# 未登陆模版
+# copy_file 'session.html.erb', 'app/views/layouts/session.html.erb'
+
+
+## 后台首页
+
+
 
 
 # before_action :authenticate_admin!
+
+
+# rails_command("db:migrate")
+# rails_command("db:seeds")
